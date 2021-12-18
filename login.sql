c7dc36e93f80ff4d295597814d04dff9d8bc5fd0
KeonJu Na <keonju2@naver.com>
2021-12-19 06:12:17 +0900

    Update login.sql

--- login.sql
+++ login.sql
@@ -16,13 +16,20 @@ INSERT INTO user (id, username, password, email,coupleID) VALUES (1, 'test', 'te
 INSERT INTO user (username, password, email,coupleID) VALUES ('test2', 'test2', 'test2@test.com','test');
 
 
-INSERT INTO posting (username,comment) VALUES ('test3', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
 
 
 CREATE TABLE IF NOT EXISTS posting (
   id int(12) NOT NULL,
   username varchar(50) NOT NULL,
   comment varchar(10000) NOT NULL,
-  createtime varchar(100) NOT NULL,
-  coupleID timestamp NOT NULL
+  year int not null,
+  month int not null,
+  day int not null
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+alter table posting add constraint user foreign key(username) references user(username);
+
+alter table posting modify id int not null auto_increment primary key;
+
+INSERT INTO posting (username,comment,year,month,day) VALUES ('test3', 'aaaaaaaaaaaaaaaaaaaaaa',21,12,10);
